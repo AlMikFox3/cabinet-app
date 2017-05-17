81ebdd19edea4e60626d97f6cc119222b142fedb
= <=>
2017-05-17 14:00:10 +0530

    Add association between User and Docs

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517081530) do
+ActiveRecord::Schema.define(version: 20170517082009) do
 
   create_table "docs", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "installs", force: :cascade do |t|
